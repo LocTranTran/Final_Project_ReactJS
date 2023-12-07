35a42529836985b9c34388c0cdc5ec8a5618390f
Thangprojl <kthang611@gmail.com>
2023-12-07 20:14:34 +0700

    cart

--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -8,21 +8,12 @@ const ProductList = ({numItem}) => {
   const { isLoading ,displayedItems } = useContext(CartContext);
    const displayedItemsSlice = displayedItems.slice(0, numItem);
   return (
-    <div className="d-flex align-items-center  flex-column  col-7 gap-5 ">
-      <h1 style={{ fontSize: "3rem" }}>Danh sách sản phẩm</h1>
-      {displayedItemsSlice.length === 0 ? (
-        <img src='https://eherbalmarket.vn/assets/images/no-cart.png' alt='lỗi'></img>
-      ):(
-      <ul className="d-flex flex-wrap gap-5">
-        {displayedItemsSlice.map((product) => (
-          <ProductsItems key={product.id} product={product} />
-        ))}
-      </ul>
-
-      )}
-      <>
-        {isLoading ? (
-          // Biểu tượng SVG trong khi đợi
+    <>
+      <h1 style={{ fontSize: "3rem", padding: "10px 0" }}>
+        Danh sách sản phẩm
+      </h1>
+      <div className="container">
+        {displayedItemsSlice.length === 0 ? (
           <div
             style={{
               display: "flex",
@@ -38,7 +29,7 @@ const ProductList = ({numItem}) => {
             />
           </div>
         ) : (
-          <ul className="d-flex flex-wrap gap-4">
+          <ul className="products__list">
             {displayedItemsSlice.map((product) => (
               <ProductsItems key={product.id} product={product} />
             ))}
