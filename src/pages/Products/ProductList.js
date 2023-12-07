dad98f463b21f78d3f0aa982a19902cfe90c695e
NguyenThoNgoc <NguyenThoNgoc.ngoc101292@donga.edu.vn>
2023-12-08 02:00:09 +0700

    them gio hàng

--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -1,52 +1,67 @@
 import React, { useContext } from "react";
 import ProductsItems from "./ProductsItems";
-import './Products.scss'
-import '../../pages/Style.scss'
+import "./Products.scss";
+import "../../pages/Style.scss";
 import { CartContext } from "../../utils/CartContext";
 import Loading from "../../utils/Loading";
-const ProductList = ({numItem}) => {
-  const { isLoading ,displayedItems } = useContext(CartContext);
-   const displayedItemsSlice = displayedItems.slice(0, numItem);
+
+const ProductList = ({ numItem }) => {
+  const { isLoading, displayedItems } = useContext(CartContext);
+  const displayedItemsSlice = displayedItems.slice(0, numItem);
+
   return (
-    <div className="d-flex align-items-center  flex-column  col-7 gap-5 ">
+    <div className="">
       <h1 style={{ fontSize: "3rem" }}>Danh sách sản phẩm</h1>
       {displayedItemsSlice.length === 0 ? (
-        <img src='https://eherbalmarket.vn/assets/images/no-cart.png' alt='lỗi'></img>
-      ):(
-      <ul className="d-flex flex-wrap gap-5">
-        {displayedItemsSlice.map((product) => (
-          <ProductsItems key={product.id} product={product} />
-        ))}
-      </ul>
-
+        <img
+          src="https://eherbalmarket.vn/assets/images/no-cart.png"
+          alt="lỗi"
+        />
+      ) : (
+        <ul className="d-flex flex-wrap gap-5">
+          {/* {displayedItemsSlice.map((product) => (
+            <ProductsItems key={product.id} product={product} />
+          ))} */}
+        </ul>
       )}
-      <>
-        {isLoading ? (
-          // Biểu tượng SVG trong khi đợi
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "400px",
-            }}
-          >
-            <img
-              style={{ width: "300px", height: "200px", marginBottom: "100px" }}
-              src="https://eherbalmarket.vn/assets/images/no-cart.png"
-              alt="lỗi"
-            />
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "400px",
+          }}
+        >
+          <img
+            style={{ width: "300px", height: "200px", marginBottom: "100px" }}
+            src="https://eherbalmarket.vn/assets/images/no-cart.png"
+            alt="lỗi"
+          />
+        </div>
+      ) : (
+        <div>
+          <section className="py-16">
+            <div className="container mx-auto">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]
+              max-w-sm mx-auto md:max-w-none md:mx-0">
+              {displayedItemsSlice.map((product) => {
+              return(
+                 <ProductsItems key={product.id} product={product} />
+              )
+            })}
+              </div>
+           
+
+            </div>
+           
+          </section>
+             
+       
+          <Loading isLoading={isLoading} />
           </div>
-        ) : (
-          <ul className="d-flex flex-wrap gap-4">
-            {displayedItemsSlice.map((product) => (
-              <ProductsItems key={product.id} product={product} />
-            ))}
-          </ul>
-        )}
-      </div>
-        <Loading isLoading={isLoading} />
-    </>
+      )}
+    </div>
   );
 };
 
--- src/pages/Products/ProductsItems.js
+++ src/pages/Products/ProductsItems.js
@@ -1,26 +1,46 @@
-import React, { memo } from 'react'
+import React, { memo,useContext } from 'react'
 import './Products.scss'
 import '../../pages/Style.scss'
+import{Link} from'react-router-dom';
+
+import { BsPlus,BsEyeFill } from 'react-icons/bs';
+
+
+
 const ProductsItems = ({ product }) => {
+  console.log(product);
+  const{id,name,images, category,price}=product;
   return (
-    <div key={product.id} className="products__items">
-      <figure className="products__items--img">
-        <img src={product.images} alt=""  className=''/>
-      </figure>
-        <div className="products__items-info">
-          <h4 className="products__items-name">{product.name}</h4>
-          <span className="products__items-price">{product.price}đ</span>
-          {/* <span className="title--info-des">{product.description}</span> */}
-        </div>
-        <button className="product__cart-button">
-          <img src="./assets/image/cart.svg" alt="" />
-        </button>
+    <div>
+        <div className='border border-[#e4e4e4] h-[300px] relative overflow-hidden group transition'>
+            <div className='w-full h-full flex justify-center items-center'>
+              {/* thêm ảnh sp dô item  */}
+                <div className='w-[200px] mx-auto flex justify-center items-center'>
+                  <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={images} alt="ảnh products"/>
+                </div>
+            </div>
+            {/* nút thêm sp và xem chi tiết sp */}
+            <div className='absolute top-0 right-0 bg-green-400 p-2 flex flex-col items-center
+            justify-center gap-y-2  group-hover:opacity-100 '>
+              <button >
+                  <div className='flex justify-center items-center text-white w-12 h-12 bg-green-600'>
+                    <BsPlus className='text-3xl'/>
+                  </div>
+              </button>
+              <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center
+               text-primary drop-shadow-xl'> <BsEyeFill/> </Link>
+            </div>
+          </div>
+          {/* ten gia mo ta */}
         <div>
-          {/* <button className="title--btn">
-            <i className="fa-solid fa-basket-shopping fa-xl"></i>
-          </button> */}
+          <Link to={`/product/${id}`}>
+          <div className='font-semibold mb-3 '>{name}</div>
+         </Link>
+          <h2 className='font-semibold'>{price}đ</h2>
+        
         </div>
     </div>
+    
   );
 }
 
